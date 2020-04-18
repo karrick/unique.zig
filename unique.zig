3ee8b8c5ef37f4847e0cffaa646d934470bb38a4
Karrick S. McDermott <karrick@karrick.net>
2020-04-17 21:21:47 -0400

    updated to work with zig 0.6.0 IO streams

--- unique.zig
+++ unique.zig
@@ -1,37 +1,31 @@
 // unique.zig
-// 
+//
 // (my first non-trivial (for me) Zig program)
 //
 // Reads from standard input, printing lines it has not yet seen. Limited to
 // lines of 4096 bytes, until I get a chance to look into better way.
 
 const std = @import("std");
-const debug = std.debug;
 
 pub fn main() !void {
-    const stdout = try std.io.getStdOut();
+    const stdin = std.io.bufferedInStream(std.io.getStdIn().inStream()).inStream();
+    const stdout = std.io.getStdOut().outStream();
 
-    var arena = std.heap.ArenaAllocator.init(std.heap.direct_allocator);
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
 
     const allocator = &arena.allocator;
-
-    var buffer = try std.Buffer.initSize(allocator, 4096);
-
     var map = std.hash_map.AutoHashMap(u64, bool).init(allocator);
     defer map.deinit();
 
-    while (true) {
-        var line = std.io.readLine(&buffer) catch |err| switch (err) {
-            error.EndOfStream => return,
-            else => |e| return e,
-        };
+    var line_buf: [4096]u8 = undefined;
 
-        var h = std.hash.Fnv1a_64.hash(line);
+    while (try stdin.readUntilDelimiterOrEof(&line_buf, '\n')) |line| {
+        if (line.len == 0) break;
 
+        const h = std.hash.Fnv1a_64.hash(line);
         if (!map.contains(h)) {
-            try stdout.write(line);
-            try stdout.write("\n");
+            try stdout.print("{}\n", .{line});
             _ = try map.put(h, true);
         }
     }

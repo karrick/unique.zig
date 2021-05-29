35f35805649f8b2e8264d42e32d5a98176c9f664
Karrick S. McDermott <karrick@karrick.dev>
2021-05-29 00:00:29 -0400

    no longer limits lines to 4096 bytes

---
+++ src/main.zig
@@ -0,0 +1,38 @@
+// unique.zig
+//
+// (my first non-trivial (for me) Zig program)
+//
+// Reads from standard input, printing lines it has not yet seen. Limited to
+// lines of 4096 bytes, until I get a chance to look into better way.
+
+const std = @import("std");
+const math = std.math;
+
+pub fn main() anyerror!void {
+    const stdin = std.io.bufferedReader(std.io.getStdIn().reader()).reader();
+    const stdout = std.io.getStdOut().writer();
+    const stderr = std.io.getStdErr().writer();
+
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = &arena.allocator;
+    var map = std.hash_map.AutoHashMap(u64, bool).init(allocator);
+    defer map.deinit();
+
+    while (true) {
+        const eline = stdin.readUntilDelimiterAlloc(allocator, '\n', math.maxInt(usize));
+        if (eline) |line| {
+            const h = std.hash.Fnv1a_64.hash(line);
+            if (!map.contains(h)) {
+                try stdout.print("{s}\n", .{line});
+                try map.put(h, true);
+            }
+        } else |err| {
+            if (err != error.EndOfStream) {
+                try stderr.print("{s}\n", .{err});
+            }
+            break;
+        }
+    }
+}
